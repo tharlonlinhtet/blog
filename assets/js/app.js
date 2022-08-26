1a54686aaac030f5b98e0e3d06dd981564df7911
Thar Lon Lin Htet <78011047+tharlonlinhtet@users.noreply.github.com>
2022-08-26 11:10:23 +0630

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+$(".show-sidebar-btn").click(function(){
+    $(".sidebar").animate({marginLeft:0});
+});
+
+$(".hide-sidebar-btn").click(function(){
+    $(".sidebar").animate({marginLeft:"-100%"});
+});
+
+function go(url){
+    setTimeout(function(){
+        location.href = `${url}`;
+    },500);
+}
+
+$(function (){
+    $('[data-toggle="popover"]').popover()
+})
+
+$(".full-screen-btn").click(function() {
+    let current = $(this).closest(".card");
+    current.toggleClass("full-screen-card");
+    if (current.hasClass("full-screen-card")){
+        $(this).html(`<i class="feather-minimize-2"></i>`);
+    }else{
+        $(this).html(`<i class="feather-maximize-2"></i>`);
+    }
+});
+
+let screenHeight = $(window).height();
+let currentMenuHeight = $(".nav-menu .active").offset().top;
+
+if (currentMenuHeight > screenHeight * 0.8){
+    $(".aside-menu").animate({
+        scrollTop:currentHeight-100
+    },1000)
+} 
